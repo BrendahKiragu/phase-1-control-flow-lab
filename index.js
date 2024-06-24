c2aef64b6f630d7cb6863bb13baff2794db597ee
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-24 16:46:40 +0300

    applies the if statements

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+  return `This one is on me!`;
+  }
+  else if (distance >400 && distance <2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 }
-
+//console.log(scuberGreetingForFeet(2300))
+  
 function ternaryCheckCity(){
   // Write your code here!
 }
